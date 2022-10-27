b909d058f85a6d1b48ed474a95bb8cc789844307
GeorgAlbert <93935543+GeorgAlbert@users.noreply.github.com>
2022-10-27 10:15:25 +0200

    Create sim.jl

---
+++ sim.jl
@@ -0,0 +1,3 @@
+# Julia code for creating and simulating the dynamics of spatially explicit food webs
+# see Albert et al. (in prep) Animal movement and plant space-use drive plant diversity-productivity relationships.
+# NOTE: code will be made available once the manuscript was accepted
