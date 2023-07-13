c1c4f76e6a196393b5b89a1325f903b529cb4146
GeorgAlbert <93935543+GeorgAlbert@users.noreply.github.com>
2023-07-13 17:34:09 +0200

    Create 0_package_install.jl

---
+++ Julia/0_package_install.jl
@@ -0,0 +1,10 @@
+# instal required Julia packages
+using Pkg
+
+Pkg.add("StatsBase")
+Pkg.add("Distributions")
+Pkg.add("LinearAlgebra")
+Pkg.add("JLD2")
+Pkg.add("DifferentialEquations")
+Pkg.add("CSV")
+Pkg.add("DataFrames")
